18cfb322a557ecb9a8506139eb041c1358e3d05c
alessky <aint.noprogrammer@gmail.com>
2023-10-21 12:48:07 +0300

    fix mangled text, while Hero animation

--- lib/ConcreteHero.dart
+++ lib/ConcreteHero.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart' show timeDilation;
 import 'HeroPage.dart';
 
 class ConcreteHero extends StatelessWidget {
--- lib/HeroPage.dart
+++ lib/HeroPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart' show timeDilation;
 
 class HeroPage extends StatelessWidget {
   HeroPage({super.key, required this.icon, required this.heroName});
@@ -6,6 +7,7 @@ class HeroPage extends StatelessWidget {
   final String heroName;
   @override
   Widget build(BuildContext context) {
+    timeDilation = 3;
     return Scaffold(
       appBar: AppBar(
         title: Text("Concrete hero: $heroName"),
@@ -31,26 +33,28 @@ class HeroPage extends StatelessWidget {
               ),
             ),
             Flexible(
-              child: SingleChildScrollView(
-                child: Container(
-                  padding: const EdgeInsets.all(30),
-                  // margin: const EdgeInsets.all(30),
-                  color: Colors.amber,
-                  child: Text(
-                    'This is a long piece of text that will be scrollable. Lorem ipsum dolor sit amet, consectetur adipiscing'
-                    ' elitorem ipsum dolor sit amet, consectetur adipiscing elitorem ipsum dolor sit amet, consectetur adipiscing '
-                    'elitorem ipsum dolor sit amet, consectetur adipiscing elitorem ipsum dolor sit amet, consectetur adipiscing '
-                    'elitorem ipsum dolor sit amet, consectetur adipiscing elitorem ipsum dolor sit amet, consectetur adipiscing '
-                    'elitorem ipsum dolor sit amet, consectetur adipiscing elitorem ipsum dolor sit amet, consectetur adipiscing '
-                    'elitorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et '
-                    'dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea '
-                    'commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla'
-                    ' pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est'
-                    ' laborum.',
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                      fontWeight: FontWeight.normal,
-                      fontSize: 20.0,
+              child: Material(
+                child: SingleChildScrollView(
+                  child: Container(
+                    padding: const EdgeInsets.all(30),
+                    // margin: const EdgeInsets.all(30),
+                    color: Colors.amber,
+                    child: Text(
+                      'This is a long piece of text that will be scrollable. Lorem ipsum dolor sit amet, consectetur adipiscing'
+                      ' elitorem ipsum dolor sit amet, consectetur adipiscing elitorem ipsum dolor sit amet, consectetur adipiscing '
+                      'elitorem ipsum dolor sit amet, consectetur adipiscing elitorem ipsum dolor sit amet, consectetur adipiscing '
+                      'elitorem ipsum dolor sit amet, consectetur adipiscing elitorem ipsum dolor sit amet, consectetur adipiscing '
+                      'elitorem ipsum dolor sit amet, consectetur adipiscing elitorem ipsum dolor sit amet, consectetur adipiscing '
+                      'elitorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et '
+                      'dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea '
+                      'commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla'
+                      ' pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est'
+                      ' laborum.',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        fontWeight: FontWeight.normal,
+                        fontSize: 20.0,
+                      ),
                     ),
                   ),
                 ),
