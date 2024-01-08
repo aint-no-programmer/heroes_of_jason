a32b07d658e11f030a06e412567040650aeb79a2
alessky <aint.noprogrammer@gmail.com>
2024-01-08 21:36:29 +0300

    fix: add PopScope to prevent backward to IntroPage

--- lib/StartPage.dart
+++ lib/StartPage.dart
@@ -8,76 +8,79 @@ class StartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('images/start_page.jpg'),
-            fit: BoxFit.fitHeight,
+      body: PopScope(
+        canPop: false,
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('images/start_page.jpg'),
+              fit: BoxFit.fitHeight,
+            ),
           ),
-        ),
-        child: Align(
-          alignment: Alignment.centerRight,
-          child: Container(
-            padding: EdgeInsets.all(40),
-            margin: EdgeInsets.all(20),
-            child: Column(
-              // crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // Expanded(
-                //   child: Spacer(),
-                // ),
-                SizedBox(
-                  height: 200,
-                ),
-                OutlinedButton(
-                  style: ButtonStyle(
-                    side: MaterialStateProperty.all(
-                      BorderSide(
-                          color: Colors.white,
-                          width: 1.0,
-                          style: BorderStyle.solid),
-                    ),
-                    foregroundColor:
-                        MaterialStateProperty.all<Color>(Colors.white),
+          child: Align(
+            alignment: Alignment.centerRight,
+            child: Container(
+              padding: EdgeInsets.all(40),
+              margin: EdgeInsets.all(20),
+              child: Column(
+                // crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Expanded(
+                  //   child: Spacer(),
+                  // ),
+                  SizedBox(
+                    height: 200,
                   ),
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/ChooseHeroPage');
-                  },
-                  child: Text('п л э й'),
-                ),
-                OutlinedButton(
-                  style: ButtonStyle(
-                    side: MaterialStateProperty.all(
-                      BorderSide(
-                          color: Colors.black,
-                          width: 1.0,
-                          style: BorderStyle.solid),
+                  OutlinedButton(
+                    style: ButtonStyle(
+                      side: MaterialStateProperty.all(
+                        BorderSide(
+                            color: Colors.white,
+                            width: 1.0,
+                            style: BorderStyle.solid),
+                      ),
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.white),
                     ),
-                    foregroundColor:
-                        MaterialStateProperty.all<Color>(Colors.black),
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/ChooseHeroPage');
+                    },
+                    child: Text('п л э й'),
                   ),
-                  onPressed: () {},
-                  child: Text('к р е а т о р с'),
-                ),
-                OutlinedButton(
-                  style: ButtonStyle(
-                    side: MaterialStateProperty.all(
-                      BorderSide(
-                          color: Colors.black,
-                          width: 1.0,
-                          style: BorderStyle.solid),
+                  OutlinedButton(
+                    style: ButtonStyle(
+                      side: MaterialStateProperty.all(
+                        BorderSide(
+                            color: Colors.black,
+                            width: 1.0,
+                            style: BorderStyle.solid),
+                      ),
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.black),
                     ),
-                    foregroundColor:
-                        MaterialStateProperty.all<Color>(Colors.black),
+                    onPressed: () {},
+                    child: Text('к р е а т о р с'),
                   ),
-                  onPressed: () => exit(0),
-                  child: Text('а у т'),
-                ),
-                // Expanded(
-                //   child: Spacer(),
-                // ),
-              ],
+                  OutlinedButton(
+                    style: ButtonStyle(
+                      side: MaterialStateProperty.all(
+                        BorderSide(
+                            color: Colors.black,
+                            width: 1.0,
+                            style: BorderStyle.solid),
+                      ),
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.black),
+                    ),
+                    onPressed: () => exit(0),
+                    child: Text('а у т'),
+                  ),
+                  // Expanded(
+                  //   child: Spacer(),
+                  // ),
+                ],
+              ),
             ),
           ),
         ),
