f2b7804dc02a72e39be8c8c0fea3e6425865e944
alessky <aint.noprogrammer@gmail.com>
2024-02-11 22:54:55 +0300

    dev: CreatorsPage done

--- lib/CreatorsPage.dart
+++ lib/CreatorsPage.dart
@@ -6,6 +6,30 @@ class CreatorsPage extends StatelessWidget {
   CreatorsPage({
     super.key,
   });
+
+  final String _alessky_description = 'стал работать писцом на пергаменте, и ему нравится. знает слово полиморфизм.';
+  final List<String> _alessky_prophecy = ['ты чего маааленький !', 'есть два аспекта'];
+  final String _alessky_creator_description = 'Alessky.\n'
+      'Programmer.\n'
+      '- game mechanics\n'
+      '- programming\n'
+      '- game scenario\n';
+
+  final String _zinke_description = 'юлия из рода юлиев. долгое время работала бартендером в тавернах по всей греции, но никогда не была в персии. потом перехала в афины и стала расписывать храмы апполона. вроде бы получается =)';
+  final List<String> _zinke_prophecy = ['7 на 8', 'Ой, как это мило!', 'или ничего...', 'это вам не фикушки воробья!', 'они думали что мы взрослые =)'];
+  final String _zinke_creator_descrition = 'J.Zinkevich.\n'
+      'Illustrator.\n'
+      '- visual design\n'
+      '- game intro\n'
+      '- heroes images\n';
+  final String _koshka_description = 'это просто кошка. из редкой породы кошек. даже немного цирковая. была подобрана юлией из рода юлиев давно где то на пелепонесах. ';
+  final List<String> _koshka_prophecy = ['мяук!', 'фррррр...'];
+  final String _koshka_creator_descrition = 'koshka.\n'
+      'cat.\n'
+      '- eat\n'
+      '- sleep\n'
+      '- murr . . . \n';
+
   //build method
   @override
   Widget build(BuildContext context) {
@@ -14,47 +38,94 @@ class CreatorsPage extends StatelessWidget {
         centerTitle: true,
         title: Text("Creators"),
       ),
-      body: Expanded(
-        child: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('images/creators_background.jpg'),
-              fit: BoxFit.fitWidth,
-            ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/creators_background.jpg'),
+            fit: BoxFit.fitWidth,
           ),
-          child: Align(
-            alignment: Alignment.centerLeft,
+        ),
+        child: Align(
+          alignment: Alignment.centerLeft,
+          child: Padding(
+            padding: EdgeInsets.all(10),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text('C R E A T O R S'),
-                Container(
-                  child: ConcreteHero(
-                    icon: 'images/creators_alessky.jpg',
-                    width: 150,
-                    heroName: 'alessky',
-                    description: 'description',
-                    prophecies: ['1', '2'],
-                  ),
-                  height: 150,
+                Text(
+                  'C R E A T O R S',
+                  style: TextStyle(fontSize: 40, color: Colors.white),
                 ),
-                Container(
-                  child: ConcreteHero(
-                    icon: 'images/creators_zinke.jpg',
-                    width: 150,
-                    heroName: 'J.Zinkevich',
-                    description: 'description',
-                    prophecies: ['1', '2'],
-                  ),
-                  height: 50,
+                SizedBox(height: 50),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ConcreteHero(
+                      icon: 'images/creators_alessky.jpg',
+                      width: 150,
+                      heroName: 'alessky',
+                      description: _alessky_description,
+                      prophecies: _alessky_prophecy,
+                    ),
+                    ConstrainedBox(
+                      constraints: BoxConstraints.loose(const Size.square(200)),
+                      child: Text(
+                        _alessky_creator_description,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15.0,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 50),
+                  ],
                 ),
-                // ConcreteHero(
-                //   icon: 'images/creators_zinke.jpg',
-                //   width: 100,
-                //   heroName: 'J.Zinkevich',
-                //   description: 'description_2',
-                //   prophecies: ['1', '2'],
-                // ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ConcreteHero(
+                      icon: 'images/creators_zinke.jpg',
+                      width: 150,
+                      heroName: 'J.Zinkevich',
+                      description: _zinke_description,
+                      prophecies: _zinke_prophecy,
+                    ),
+                    ConstrainedBox(
+                      constraints: BoxConstraints.loose(const Size.square(200)),
+                      child: Text(
+                        _zinke_creator_descrition,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15.0,
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 30),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ConcreteHero(
+                      icon: 'images/creators_koshka.jpg',
+                      width: 150,
+                      heroName: 'koshka',
+                      description: _koshka_description,
+                      prophecies: _koshka_prophecy,
+                    ),
+                    ConstrainedBox(
+                      constraints: BoxConstraints.loose(const Size.square(200)),
+                      child: Text(
+                        _koshka_creator_descrition,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15.0,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 50),
+                  ],
+                ),
               ],
             ),
           ),
